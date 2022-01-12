e1eb04669b0cb15ea0bdcb4f4107458f6caa210b
mayconlean1 <mayconlean@hotmail.com>
2022-01-11 21:51:34 -0300

    create layout 1 condition

--- script.js
+++ script.js
@@ -18,57 +18,95 @@ const gearsAvailable = [
 
 
 function createGearsLayout(gearsAvailable = []){
-    const gearsLayout = { ...generateAssemblies('layout_0')}
+    const gearsLayout = { 
+        ...generateAssemblies('layout_0'),
+        ...generateAssemblies('layout_1'),
+    }
     
     function generateAssemblies(layout=''){
-
         const gearsLayout = {}
-    
-        gearsAvailable.forEach((a1 ,a1i)=>{
-            const remainingGears = [...gearsAvailable]
-            remainingGears.splice(a1i, 1)
-    
-            let gearCombination = {a:[undefined, a1]}
-    
-            remainingGears.forEach((b1, b1i)=>{
-    
-                remainingGears.splice(b1i, 1)
-                gearCombination["b"] = [undefined, b1]
-    
-                remainingGears.forEach((c1, c1i)=>{
-    
-                    remainingGears.splice(c1i, 1)
-    
-                    gearCombination["c"] = [null, c1]
-                    if(!gearsLayout[0]){
-                        gearsLayout[0] = []
-                    }
-                    gearsLayout[0].push(gearCombination)
-    
-                    gearCombination= {a:[undefined, a1], b:[undefined,b1]}
-                    
+
+        if(layout == 'layout_0'){
+            gearsAvailable.forEach((a1 ,a1i)=>{
+                const remainingGears = [...gearsAvailable]
+                remainingGears.splice(a1i, 1)
+        
+                let gearCombination = {a:[undefined, a1]}
+        
+                remainingGears.forEach((b1, b1i)=>{
+        
+                    remainingGears.splice(b1i, 1)
+                    gearCombination["b"] = [undefined, b1]
+        
+                    remainingGears.forEach((c1, c1i)=>{
+        
+                        remainingGears.splice(c1i, 1)
+        
+                        gearCombination["c"] = [null, c1]
+                        if(!gearsLayout[0]){
+                            gearsLayout[0] = []
+                        }
+                        gearsLayout[0].push(gearCombination)
+        
+                        gearCombination= {a:[undefined, a1], b:[undefined,b1]}
+                        
+                    })
                 })
+        
             })
+        }else if(layout == 'layout_1'){
+            gearsAvailable.forEach((a0 ,a0i)=>{
+                const remainingGears = [...gearsAvailable]
+                remainingGears.splice(a0i, 1)
+        
+                let gearCombination = {a:[a0]}
+        
+                remainingGears.forEach((a1, a1i)=>{
+        
+                    remainingGears.splice(a1i, 1)
+                    gearCombination["a"].push(a1) 
+        
+                    remainingGears.forEach((b0, b0i)=>{
+                        remainingGears.splice(b0i, 1)
+                        gearCombination["b"] = [b0, undefined] 
+
+                        remainingGears.forEach((c0, c0i)=>{
+        
+                            remainingGears.splice(c0i, 1)
+                            gearCombination["c"] = [c0, null]
+
+                            if(!gearsLayout[1]){
+                                gearsLayout[1] = []
+                            }
+                            gearsLayout[1].push(gearCombination)
+            
+                            gearCombination= {a:[a0, a1], b:[b0,undefined]}
+                            
+                        })
+                    })
+                })
+        
+            })
+        }
     
-        })
 
         return gearsLayout
 
 
     }
     
-    console.log( gearsLayout)
+    // console.log( gearsLayout)
     
-    // gearsLayout[0].forEach(layout =>{
-    //     gearsLayout0.push(createGearsData({gearPosition:layout}))
-    // })
-
     return  gearsLayout
+    
 }
-
 const gearsLayout = createGearsLayout(gearsAvailable)
+const gearsData = []
+gearsLayout[1].forEach(layout =>{
+    gearsData.push(createGearsData({gearPosition:layout}))
+})
 
-console.log(gearsLayout[70].getSchemas())
+console.log(gearsData[70].getSchemas())
 
 // const gearPosition = {
 //     'a': [50 , 80],
