28424ccabd718852e11ac4588d6854dc13fe365f
mayconlean1 <mayconlean@hotmail.com>
2022-02-03 13:01:43 -0300

    install ejs and create public file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require ('express');
 const app = express();
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname + '/views'))
+app.use( express.static( 'public' ))
 
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-    return res.status(400).send('ok')
+    return res.status(400).render('index')
 })
 
 module.exports = app
