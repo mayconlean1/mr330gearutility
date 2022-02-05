54752cf415f2a86301829d1284efc7a925a2c688
mayconlean1 <mayconlean@hotmail.com>
2022-02-05 14:26:04 -0300

    created add gear in select , remove gear of select, init search function

---
+++ public/scripts/script.js
@@ -0,0 +1,41 @@
+let gearsAvailable = []
+
+function addGear(){
+    const inputValue = document.querySelector('#inputAddGear').value
+
+    const newOption = document.createElement('option')
+    newOption.value = inputValue
+    newOption.textContent = inputValue
+
+    const select = document.querySelector('#select_gearsAvaliable')
+    console.log('add', select)
+    select.appendChild(newOption)
+}
+
+function removeGear(){
+    const select = document.querySelector('#select_gearsAvaliable')
+    select.remove(select.selectedIndex)
+
+}
+
+function search(){
+    
+    createGearsArray()
+
+
+    function createGearsArray(){
+        const select = document.querySelector('#select_gearsAvaliable')
+        const selectGearsAvailable = select.options
+        gearsAvailable = []
+    
+        for (let index in selectGearsAvailable){
+            const gear = selectGearsAvailable[index]
+    
+            if (gear.value){
+                gearsAvailable.push(gear.value)
+            }
+        }
+    }
+
+    console.log(gearsAvailable)
+}
